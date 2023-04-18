7d1dcdfbab69a7c59783658b9aee39d64f106ca3
MrSomFeRGO <mr.somfergo@gmail.com>
2023-04-18 17:33:53 +0300

    1

--- GDTask-2/Program.cs
+++ GDTask-2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿object[] array = { 1, "2", 3, "4", "5" };
+
+int sum = 0;
+int count = 0;
+
+foreach (object element in array)
+{
+    if (element is int)
+    {
+        sum += (int)element;
+    }
+
+    if (element is string)
+    {
+        count++;
+    }
+}
+
+int result = sum - count;
+
+Console.WriteLine(result);
